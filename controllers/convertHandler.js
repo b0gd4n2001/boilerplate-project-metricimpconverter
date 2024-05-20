83b1b25d101bd08b50b8dbf54001b718973f42ee
b0gd4n2001 <lulelarub@gmail.com>
2024-05-20 15:15:41 +0300

    feat: only accept valid initial number and unit

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -1,25 +1,22 @@
 function ConvertHandler() {
   
   this.getNum = function(input) {
-    let result;
-    
-    return result;
+    let num = input.match(/^\d+(?:\.\d+)?(?:\/\d+(?:\.\d+)?)?(?=gal|l|lbs|kg|mi|km)/i);
+    let unit = input.match(/(?<=\d)(?:gal|l|lbs|kg|mi|km)$/i);
+    if (unit == null) return "invalid unit";
+    if (num == null && input.length != unit[0].length) return "invalid number";
+    num = num ? num[0] : 1;
+    return num;
   };
   
   this.getUnit = function(input) {
-    let result;
+    let result = input;
     
     return result;
   };
   
   this.getReturnUnit = function(initUnit) {
-    let result;
-    
-    return result;
-  };
-
-  this.spellOutUnit = function(unit) {
-    let result;
+    let result = initUnit;
     
     return result;
   };
@@ -28,13 +25,13 @@ function ConvertHandler() {
     const galToL = 3.78541;
     const lbsToKg = 0.453592;
     const miToKm = 1.60934;
-    let result;
+    let result = [initNum, initUnit];
     
     return result;
   };
   
   this.getString = function(initNum, initUnit, returnNum, returnUnit) {
-    let result;
+    let result = `${initNum}${initUnit}${returnNum}${returnUnit}`;
     
     return result;
   };
--- routes/api.js
+++ routes/api.js
@@ -4,7 +4,19 @@ const expect = require('chai').expect;
 const ConvertHandler = require('../controllers/convertHandler.js');
 
 module.exports = function (app) {
-  
+
   let convertHandler = new ConvertHandler();
+
+  app.route('/api/convert/:input?').get((req, res) => {
+    const input = req.param.input ?? req.query.input;
+    const answer = {
+      initNum: convertHandler.getNum(input),
+      initUnit: convertHandler.getUnit(input),
+    };
+    answer.returnNum = convertHandler.convert(answer.initNum, answer.initUnit);
+    answer.returnUnit = convertHandler.getReturnUnit(answer.initUnit);
+    answer.string = convertHandler.getString(answer.initNum, answer.initUnit, answer.returnNum, answer.returnUnit);
+    res.json(answer);
+  })
 
 };
